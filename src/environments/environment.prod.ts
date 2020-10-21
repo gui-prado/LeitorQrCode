4c7719407cbcbbb86c76164b49730bda3702f64d
Guilherme Rodrigues Prado <gui.prado@protonmail.com>
2020-10-20 22:12:41 -0300

    Aula 7
    Aplicativo finalizado até a aula 7.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC0WJyRUY9agsfIzZEU2irqv4lrDSv2eLM",
+    authDomain: "leitorqrcodefirebase-f5a5a.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefirebase-f5a5a.firebaseio.com",
+    projectId: "leitorqrcodefirebase-f5a5a",
+    storageBucket: "leitorqrcodefirebase-f5a5a.appspot.com",
+    messagingSenderId: "648450654063",
+    appId: "1:648450654063:web:dce897519aa4567c05f29a"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC0WJyRUY9agsfIzZEU2irqv4lrDSv2eLM",
+    authDomain: "leitorqrcodefirebase-f5a5a.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefirebase-f5a5a.firebaseio.com",
+    projectId: "leitorqrcodefirebase-f5a5a",
+    storageBucket: "leitorqrcodefirebase-f5a5a.appspot.com",
+    messagingSenderId: "648450654063",
+    appId: "1:648450654063:web:dce897519aa4567c05f29a"
+  }
 };
 
 /*
